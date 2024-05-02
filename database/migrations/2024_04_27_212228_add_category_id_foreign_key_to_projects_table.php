38c817727a4d2fdb2628940e1efde73356e8beca
Luca <lucafagiolini@live.it>
2024-05-02 12:47:23 +0200

    Bug fixed: Category foreign key migration fixed

--- database/migrations/2024_04_27_212228_add_category_id_foreign_key_to_projects_table.php
+++ database/migrations/2024_04_27_212228_add_category_id_foreign_key_to_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->foreignId('category_id')->nullable()->costrained();
+            $table->foreignId('category_id')->nullable()->constrained();
         });
     }
 
